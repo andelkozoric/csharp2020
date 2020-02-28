0abf9234c75aa270ae621be2a5084a4ca59d16a0
office10 <office10@ucione.local>
2020-02-28 20:51:32 +0100

    28.02. - 20:51

---
+++ azoric/8_2_2_2_out/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _8_2_2_2_out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a; // primjer bez vrijednosti
+            int b = 88; // primjer sa vrijednosti
+            inicijaliziraj(out a);
+            Console.WriteLine("a je jednako" + a);
+
+            inicijaliziraj(out b);
+            Console.WriteLine("b je jednako" + b);
+
+
+        }
+
+        private static void inicijaliziraj(out int a)
+        {
+            a = 55;
+        }
+    }
+}
---
+++ azoric/9_1_1_znak_u_rijeci/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _9_1_1_znak_u_rijeci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("9_1_1 znak u rijeci, unesi rijeci i jedan znak iz rijeci");
+            string rijec = Console.ReadLine();
+            string znak = Console.ReadLine();
+            int brojznakova = 0;
+
+            //Substring (int startIndex);
+            //int IndexOf(String value);
+
+            string r = rijec;
+            while (r.IndexOf(znak) != -1)
+            {
+                brojznakova++;
+                r=r.Substring(r.IndexOf(znak)+1);
+            }
+            Console.WriteLine($"Broj znakova {znak} u recenici je {brojznakova}");
+
+            //drugi nacin
+            brojznakova = 0;
+            for (int i = 0; i <rijec.Length; i++)
+            {
+                if(rijec[i] == char.Parse(znak))
+                {
+                    brojznakova++;
+                }
+            }
+            Console.WriteLine($"Broj znakova {znak} u recenici je {brojznakova}");
+
+
+        }
+    }
+}
