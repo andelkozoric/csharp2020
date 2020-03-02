2de8a139a84d0088353f03082955d6ab6b352340
AnđelkoZorić <60152573+andelkozoric@users.noreply.github.com>
2020-03-02 18:20:51 +0100

    poglavlje datoteke i 2 zadataka

---
+++ azoric/10.1.1. parni/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+
+namespace _10._1._1._parni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Parni brojevi od 1 do 100");
+            FileStream fs = new FileStream("PARNI.txt", FileMode.Create);
+
+            StreamWriter sw = new StreamWriter(fs);
+
+            for (int i = 1; i <= 100; i++)
+                if (i % 2 == 0) 
+                {
+                    sw.WriteLine(i);
+                    
+                }
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+
+            fs = new FileStream("PARNI.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+        }
+    }
+}
