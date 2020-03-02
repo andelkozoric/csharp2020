32b58f93c20c91bd7e41f12da5e10642d491f341
AnđelkoZorić <60152573+andelkozoric@users.noreply.github.com>
2020-03-02 17:06:16 +0100

    poglavlje Datoteke - primjeri iz prirucnika

---
+++ azoric/10.1.1.primjer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace _10._1._1.primjer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("upisite teksto");
+            string upisano = Console.ReadLine();
+
+            //Zapisivanje u datoteku
+            FileStream fs1 = new FileStream("Proba.txt", FileMode.Create);
+
+            StreamWriter sw = new StreamWriter(fs1);
+
+            sw.WriteLine(upisano);
+
+            sw.Flush();
+
+            sw.Close();
+            fs1.Close();
+
+            //Citanje dadoteke
+            FileStream fs2 = new FileStream("proba.txt", FileMode.Open);
+
+            StreamReader sr = new StreamReader(fs2);
+
+            string procitano = sr.ReadToEnd();
+            sr.Close();
+            fs2.Close();
+
+            Console.WriteLine(procitano);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ azoric/10.1.2.primjer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace _10._1._2.primjer
+{
+    class Program
+    {
+        private static string mojaPutanja;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mapa u kojoj se izvrsava nas program: ");
+            Console.WriteLine(Environment.CurrentDirectory, "Proba.txt");
+
+            Console.Write("Postoji li file Proba.txt u izvrsnoj mapi: ");
+            //Path klasa
+            Console.WriteLine(File.Exists(mojaPutanja));
+
+            //Directory klasa
+            if(!Directory.Exists("Proba"))
+            {
+                //Ako ne postoji folder Proba, kreiram ga
+                Directory.CreateDirectory("Proba");
+            }
+
+            //Ispisujem sve foldere na C-u
+            Console.WriteLine("Direktoriji na C:");
+            string[] sDirs = Directory.GetDirectories(@"C:\");
+            foreach (string sDir in sDirs)
+            {
+                Console.WriteLine(sDir);
+            }
+
+            //Ispisujem sve fileove na C-u
+            Console.WriteLine("Datoteke na C:");
+            string[] sFiles = Directory.GetFiles(@"C:\");
+            foreach (string sFile in sFiles)
+            {
+                Console.WriteLine(Path.GetFileName(sFile));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
