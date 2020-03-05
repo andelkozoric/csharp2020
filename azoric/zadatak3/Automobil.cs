eda1c4c9634e83129c8412fcc34d1d70d078429b
office10 <office10@ucione.local>
2020-03-05 20:53:14 +0100

    zadatci sa ispita

---
+++ azoric/zadatak3/Automobil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace zadatak3
+{
+    internal class Automobil
+    {
+        public string Naziv { get; internal set; }
+        public int GodinaProizvodnje { get; internal set; }
+        public double OsnovnaCijena { get; internal set; }
+
+        public double Starost()
+        {
+            return (GodinaProizvodnje - 2020);
+        }
+    }
+    }
---
+++ azoric/zadatak3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace zadatak3
+{
+    class Program
+    {
+        
+        
+
+        static void Main(string[] args)
+        {
+            
+
+            Automobil a1 = new Automobil();
+
+            Console.WriteLine("Unesite naziv automobila");
+            a1.Naziv = Console.ReadLine();
+
+            Console.WriteLine("Unesite godinu proizvodnje");
+            a1.GodinaProizvodnje = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite osnovnu cijenu");
+            a1.OsnovnaCijena = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Automobil je {a1.Naziv}, godina proizvodnje je {a1.GodinaProizvodnje}, cijena je {a1.OsnovnaCijena}");
+
+
+            //Metode
+            a1.Starost();
+
+            
+            
+
+
+            
+        }
+
+
+      
+    }
+}
