69c36034750b63d1a9505974ca860d0bba80d67c
AnđelkoZorić <60152573+andelkozoric@users.noreply.github.com>
2020-03-12 21:58:58 +0100

    zadatci

--- azoric/9.1.6.palindrom/Program.cs
+++ azoric/9.1.6.palindrom/Program.cs
@@ -6,7 +6,32 @@ namespace _9._1._6.palindrom
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // napisite program koji trazi unos jedne linije teksta i zatim ispituje je li taj tekst "palindrom"(pali9ndrom je rijec koja ista čitamo li je s lijeve il desne strane
+            Console.WriteLine("Unesite tekst");
+
+            string text = Console.ReadLine();
+            text = text.Trim();
+            text = text.Replace(" ", "");
+            text = text.ToLower();
+
+            string reversedText = "";
+
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                reversedText += text[i];
+            }
+            Console.WriteLine(reversedText);
+
+            if (text.Equals(reversedText))
+            {
+                Console.WriteLine(text + " je palindrom!");
+            }
+            else
+            {
+                Console.WriteLine(text + " NIJE je palindrom!");
+            }
+
+
         }
     }
 }
--- azoric/9.1.7.osoba/Program.cs
+++ azoric/9.1.7.osoba/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace _9._1._7.osoba
 {
@@ -6,7 +8,18 @@ namespace _9._1._7.osoba
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //u glavnom programu instanciramo novi objekt klase Covjek
+            Covjek covjek = new Covjek();
+
+            //i trazimo unos imena i prezime
+            Console.Write("Unesite ime covjeka!");
+
+            covjek.Ime = Console.ReadLine();
+
+            Console.Write("Unesite prezime covjeka!");
+
+            covjek.Prezime = Console.ReadLine();
+
         }
     }
 }
